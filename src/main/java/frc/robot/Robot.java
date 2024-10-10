c9c36b029b792d2bc2d80ed6ffefd95acbf5a2c9
Nathan Pulsipher <npulsipher4@gmail.com>
2024-10-09 18:32:02 -0700

    Change to base functionality

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,17 +5,27 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.arm;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
 
+  final arm myArmProto = new arm();
+  final XboxController myController = new XboxController(0);
+
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
+
+    Trigger buttonX = new Trigger(myController::getXButtonPressed);
+    Trigger buttonB = new Trigger(myController::getBButtonPressed);
+
   }
 
   @Override
--- src/main/java/frc/robot/subsystems/arm.java
+++ src/main/java/frc/robot/subsystems/arm.java
@@ -8,85 +8,22 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class arm extends SubsystemBase {
   PWM m_armMotor = new PWM(Constants.kArmPWMid);
   DutyCycleEncoder m_armEncoder = new DutyCycleEncoder(Constants.kEncoderChannel);
+  Spark m_armMotorSpark = new Spark(Constants.kEncoderChannel);
   PIDController m_armPIDController = new PIDController(Constants.kP, Constants.kI, Constants.kD);
-  double m_relativeTimeOffset = 0.0;
-  double m_startVelocity = findCurrentVelocity();
-  boolean m_prevCycleAcceleration = false;
-  double prevDistance = 0;
 
   public arm() {
     m_armEncoder.setDistancePerRotation(Constants.kArmDistancePerRot);
-    prevDistance = m_armEncoder.getAbsolutePosition();
-  }
-
-
-  private double findDecelThreshold(double speed) {
-    if (speed > 0) {
-      return Constants.kTargetDistance - Constants.kDecelThresholdRel;
-    }
-    return Constants.kTargetDistance + Constants.kDecelThresholdRel;
-  }
-
-  public double findCurrentVelocity() {
-    double vel = (m_armEncoder.getAbsolutePosition() - prevDistance) / Constants.kDeltaT;
-    prevDistance = m_armEncoder.getAbsolutePosition();
-    return vel;
-
-  }
-
-  private double getRelativeTime() {
-    return (double)RobotController.getFPGATime() - m_relativeTimeOffset;
-  }
-
-  private boolean shouldBeAccelerating(double targetVelocity) {
-    if (targetVelocity + (targetVelocity * Constants.kAccelerationTolerance) > findCurrentVelocity() &&
-      targetVelocity - (targetVelocity * Constants.kAccelerationTolerance) < findCurrentVelocity()) {
-        return false;
-    }
-    return true;
-  }
-
-  private double totalDistance(double joystickSpeed, double deltaV) {
-    double distance = (joystickSpeed * deltaV) / 2.0;
-    double position = m_armEncoder.getAbsolutePosition();
-    if ((position > Constants.kTargetDistance && position + distance < Constants.kTargetDistance) ||
-      (position < Constants.kTargetDistance && position + distance > Constants.kTargetDistance)){
-
-    }
-  }
-
-  private double calculateVelocity(double joystickSpeed) {
-    double targetVelocity = joystickSpeed * Constants.kMaxVelocity;
-    if ((findCurrentVelocity() > targetVelocity && targetVelocity >= 0.0) ||
-      (findCurrentVelocity() < targetVelocity && targetVelocity <= 0.0) ||
-      (!m_prevCycleAcceleration && shouldBeAccelerating(targetVelocity))) {
-        m_relativeTimeOffset = (double)RobotController.getFPGATime() / 1000000.0;
-        m_startVelocity = findCurrentVelocity();
-    }
-    double deltaV = targetVelocity - m_startVelocity;
-    double distanceScalingFactor = (Constants.kFinishedAccelerationPeriod * (deltaV/Constants.kMaxVelocity));
-
-    m_prevCycleAcceleration = shouldBeAccelerating(targetVelocity);
-
-    return (
-      3 * deltaV * Math.pow(
-        getRelativeTime()/distanceScalingFactor,
-        2) + 
-      -2 * deltaV * Math.pow( 
-        getRelativeTime()/distanceScalingFactor,
-        3) + 
-      m_startVelocity
-    );
   }
 
   public void moveArm(double joystickSpeed) {
-      calculateVelocity(joystickSpeed);
+      
   }
 
   @Override
